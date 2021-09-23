60e08503592f038f07c626961f7aa9aafa3d8a26
RightHennessy <myheaven_b@naver.com>
2021-09-23 21:54:23 +0900

    [Unit 2] pathway3 : Display a scrollable list : Affirmations

---
+++ RightHennessy/Affirmations/app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.affirmations
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmations.adapter.ItemAdapter
+import com.example.affirmations.data.Datasource
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // 데이터 초기화
+        val myDataset = Datasource().loadAffirmations()
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+
+        // RecyclerView 사이즈 변경 금지지
+       recyclerView.setHasFixedSize(true)
+    }
+}
